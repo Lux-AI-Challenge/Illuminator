80355138b72d7ba8ca2f4b8f35461d4bf6b61076
William Duan <38791932+duanwilliam@users.noreply.github.com>
2021-11-15 00:15:04 -0800

    refactor!(ipc): port preload to TS
    - refactor preload script to use new ipc types/api
    - add webpack config to transpile preload to js before running

---
+++ src/main/preload.ts
@@ -0,0 +1,104 @@
+import 'regenerator-runtime/runtime';
+import { contextBridge, ipcRenderer } from 'electron';
+import { invokeFunc } from './ipc/wrapper';
+import type { Dimensions } from './ipc/dimensions/dimensions';
+import type { User } from './ipc/user/user';
+import type { System } from './ipc/system/system';
+
+const handlers: Window['electron'] = {
+  store: {
+    get(val) {
+      return ipcRenderer.invoke('electron-store-get', val);
+    },
+    set(property, val) {
+      return ipcRenderer.invoke('electron-store-set', property, val);
+    },
+  },
+  dimensions: {
+    makeEnv(data) {
+      return invokeFunc<Dimensions, 'makeEnv'>('dim_makeEnv', data);
+    },
+    closeEnv(data) {
+      return invokeFunc<Dimensions, 'closeEnv'>('dim_closeEnv', data);
+    },
+    runEpisode(data) {
+      return invokeFunc<Dimensions, 'runEpisode'>('dim_runEpisode', data);
+    },
+    runSingleEpisode(data) {
+      return invokeFunc<Dimensions, 'runSingleEpisode'>(
+        'dim_runSingleEpisode',
+        data
+      );
+    },
+    initializeAgents(data) {
+      return invokeFunc<Dimensions, 'initializeAgents'>(
+        'dim_initializeAgents',
+        data
+      );
+    },
+    envRegisterAgents(data) {
+      return invokeFunc<Dimensions, 'envRegisterAgents'>(
+        'dim_envRegisterAgents',
+        data
+      );
+    },
+  },
+  user: {
+    getPreferences(data) {
+      return invokeFunc<User, 'getPreferences'>('user_getPreferences', data);
+    },
+    setPreferences(data) {
+      return invokeFunc<User, 'setPreferences'>('user_setPreferences', data);
+    },
+  },
+  system: {
+    getPythonInterpreters(data) {
+      return invokeFunc<System, 'getPythonInterpreters'>(
+        'sys_getPythonInterpreters',
+        data
+      );
+    },
+    fileSelect(data) {
+      return invokeFunc<System, 'fileSelect'>('sys_fileSelect', data);
+    },
+  },
+  ipcRenderer: {
+    on(channel, func) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    },
+    once(channel, func) {
+      ipcRenderer.once(channel, (event, ...args) => func(...args));
+    },
+  },
+};
+
+/* eslint-disable */
+const wrapErrors = (handlers: any) => {
+  /**
+   * wrap all the preloaded functions so that they can return errors to then get thrown in the renderer instead of throwing in the main process
+   *
+   * by avoiding throwing in the main process, main process is not always printing something.
+   *
+   * main process can still throw errors and that will then just raise an error on the main process (which should never happen).
+   */
+  for (const key in handlers) {
+    if (!(handlers[key] instanceof Function)) {
+      wrapErrors(handlers[key]);
+    } else {
+      // is a function
+      const oldfunc = handlers[key];
+      handlers[key] = async (...args: Parameters<typeof oldfunc>) => {
+        const res = await oldfunc(...args);
+        if (res instanceof Error) {
+          throw res;
+        } else {
+          return res;
+        }
+      };
+    }
+  }
+};
+/* eslint-enable */
+
+wrapErrors(handlers);
+contextBridge.exposeInMainWorld('electron', handlers);
